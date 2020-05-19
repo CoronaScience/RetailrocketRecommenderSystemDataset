06faf2f97b95b8386eced85ddc76e0204019c2db
Angel Camarero <angel.luis.camarero.sierra@pragsis.com>
2020-05-19 11:52:54 +0200

    No important changes

--- src/main/test.py
+++ src/main/test.py
@@ -1,4 +1,21 @@
-from pyspark_config.main.process.process import Process
+import tempfile
 
-config="/home/patrizio/PycharmProjects/recommendator/src/main/resource/config_test.yaml"
-Process(config, "hola").output
+from pyspark_config import Config
+from pathlib import Path
+from pyspark_config.output import *
+from pyspark_config.input import *
+from pyspark_config.spark_utils.dataframe_extended import DataFrame_Extended
+from pyspark_config.transformations.transformations import *
+from pyspark.sql import SparkSession
+
+
+
+print("Reading path...")
+conf = Config()
+conf.load(Path("resource/config_test.yaml"))
+print("Path readed...")
+print(conf)
+
+print(tempfile.gettempdir())
+
+conf.apply()
