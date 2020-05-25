ce2119d80e8ba196f600742f4fe03651e8a1963f
Angel Camarero <angel.luis.camarero.sierra@pragsis.com>
2020-05-25 17:52:41 +0200

    path added

--- src/main/python/Input/input.py
+++ src/main/python/Input/input.py
@@ -1,3 +1,6 @@
+import os
+import pathlib
+
 from petastorm import make_batch_reader
 from petastorm.tf_utils import make_petastorm_dataset
 from pyspark_config import Config
@@ -15,10 +18,30 @@ class Input(object):
         path_='file:/'+path
         with make_batch_reader(path_, num_epochs=1) as reader:
             train_dataset = make_petastorm_dataset(reader).map(lambda x: x.item_list).unbatch()
+
+            feature_length = 10
+            label_length = 5
+
+            range_ds = tf.data.Dataset.range(100000)
+
+            def dense_1_step(batch):
+                # Shift features and labels one step relative to each other.
+                return batch[:-1], batch[1:]
+
+            predict_dense_1_step = train_dataset.map(dense_1_step)
+
+            for features, label in predict_dense_1_step.take(3):
+                print(features.numpy(), " => ", label.numpy())
+
             for x in train_dataset:
                 print(x)
 
-petastorm_dataset_url = "//home/patrizio/PycharmProjects/RetailrocketRecommenderSystemDataset/src/main/python/Input/Output/test.parquet"
+print("\nIf you mean the current working directory:\n")
+print(str(pathlib.Path().absolute()).replace("\\","/").replace("C:/","//")+"/Output/test.parquet")
+
+
+
+petastorm_dataset_url = str(pathlib.Path().absolute()).replace("\\","/").replace("C:/","//")+"/Output/test.parquet"
 
 #Input.config_load("input_pipeline.yaml")
 Input.get_dataset(petastorm_dataset_url, shuffle=100, batch=2)
